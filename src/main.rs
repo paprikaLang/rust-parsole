1f41fbe6aff87e330d4cfd4daeb73b2510f0969d
paprikaLang <3060866223@qq.com>
2018-01-22 11:22:51 +0800

    json解析

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
 extern crate clap;
 extern crate reqwest;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
 pub static URL: &str = "https://newsapi.org/v2/sources?language=en&apiKey=c16fc7641ca44e0f85995fbbe7a5ae09";
 use clap::App;
 
-fn get_feed() -> String {
+#[derive(Debug,Deserialize,Serialize)]
+struct FeedItem {
+    id: String,
+    name: String,
+    description: String,
+    url: String,
+    category: String,
+    language: String,
+    country: String
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Feed {
+    status: String,
+    sources: Vec<FeedItem>,
+}
+
+
+fn get_feed() -> Feed {
     let client = reqwest::Client::new();
     let mut request = client.get(URL);
+
     let mut resp = request.send().unwrap();
+
     assert!(resp.status().is_success());
-    return resp.text().unwrap();
+
+    let data = resp.text().unwrap();
+
+    serde_json::from_str(&data).unwrap()
 }
 
 fn main() {
