459c8615eaef06dd9536ee71656e28a7aa8c4ba8
paprikaLang <3060866223@qq.com>
2018-01-22 08:04:14 +0800

    添加clap命令行工具

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+	let app = App::new("parsole")
+	    .version("0.1")
+	    .author("paprikaLang")
+	    .about("Read JSON")
+	    .arg_from_usage("-n, --number=[NUMBER] 'Only print the NUMBER most recent posts'
+	    	             -c, --count           'Show the count of posts'");
+	let _matches = app.get_matches();
+    
 }
