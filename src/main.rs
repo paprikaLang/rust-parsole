81fbda136c3d867cf6c98d429ca8909ee70b063c
paprikaLang <3060866223@qq.com>
2018-01-22 09:38:21 +0800

    添加reqwest发送请求

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 extern crate clap;
+extern crate reqwest;
+
+pub static URL: &str = "https://newsapi.org/v2/sources?language=en&apiKey=c16fc7641ca44e0f85995fbbe7a5ae09";
 use clap::App;
+
+fn get_feed() -> String {
+    let client = reqwest::Client::new();
+    let mut request = client.get(URL);
+    let mut resp = request.send().unwrap();
+    assert!(resp.status().is_success());
+    return resp.text().unwrap();
+}
 
 fn main() {
 	let app = App::new("parsole")
@@ -9,5 +20,6 @@ fn main() {
 	    .arg_from_usage("-n, --number=[NUMBER] 'Only print the NUMBER most recent posts'
 	    	             -c, --count           'Show the count of posts'");
 	let _matches = app.get_matches();
+    let _feed = get_feed();
     
 }
